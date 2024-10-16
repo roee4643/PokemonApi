57fb5f9ac0d9f7f9a6fc393756cd617774de6952
roei ovadia <roee4643@gmail.com>
2024-10-16 15:26:18 +0300

    Update UserData.sh

--- UserData.sh
+++ UserData.sh
@@ -7,19 +7,18 @@ sudo yum install -y git
 cd 
 mkdir App
 cd App
-git clone https://github.com/roee4643/gitLab
+git clone https://github.com/roee4643/PokemonApi
 sudo python3 -m ensurepip --upgrade
 sudo /usr/bin/python3 -m pip install --upgrade pip
 sudo chmod u+w /App/gitLab/PokemonDB.json
-sudo chown ec2-user /App/gitLab/PokemonDB.json
+sudo chown ec2-user /App/PokemonApi/PokemonDB.json
 
 
 
-#sudo yum install -y python3-venv
-#python3 -m venv venv
-#source venv/bin/activate
-#pip3 install -r requirements.txt
-#python3 PokemonMainApi.py   # Replace with your app's entry point
+#sudo python3 -m venv myenv
+#sudo source myenv/bin/activate
+#sudo pip install boto3
+
 
 
 
