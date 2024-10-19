ddbebc2f2ff4a120f9f0090b34ca144f03419a6c
roei ovadia <roee4643@gmail.com>
2024-10-19 21:05:12 +0300

    Update UserData.sh

--- UserData.sh
+++ UserData.sh
@@ -6,19 +6,35 @@ sudo yum update -y   # For Amazon Linux or CentOS
 sudo yum install -y git
 cd 
 mkdir App
+sudo mkdir .aws
 cd App
 git clone https://github.com/roee4643/PokemonApi
+cd ..
+
+sudo touch .aws/credentials
+
+
+sudo mv App home/ec2-user/
+sudo mv .aws home/ec2-user/
+
+
+
 sudo python3 -m ensurepip --upgrade
 sudo /usr/bin/python3 -m pip install --upgrade pip
-sudo chmod u+w /App/gitLab/PokemonDB.json
-sudo chown ec2-user /App/PokemonApi/PokemonDB.json
+
+pip3 install boto3
+
+# Add the AWS credentials as environment variables to the profile
+echo "export AWS_ACCESS_KEY_ID=" >> /etc/profile
+echo "export AWS_SECRET_ACCESS_KEY=" >> /etc/profile
+echo "export AWS_SESSION_TOKEN=" >> /etc/profile
+echo "export AWS_DEFAULT_REGION=us-west-2" >> /etc/profile
+
+# Apply the changes
+source /etc/profile
 
 
-
-#sudo python3 -m venv myenv
-#sudo source myenv/bin/activate
-#sudo pip install boto3
-
-
-
+cd /home/ec2-user/App/PokemonApi
+python3 create_databaseDynamo.py
+python3 PokemonMainApi2.py
 
